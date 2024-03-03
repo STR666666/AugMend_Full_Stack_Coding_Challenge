de80087e94ae7edf4523ffcf2735b3d79fba7ea8
Scott R <renscott66@gmail.com>
2024-03-02 19:37:21 -0800

    google log in is successful

--- react_website/src/App.js
+++ react_website/src/App.js
@@ -1,12 +1,33 @@
+import React from 'react';
 import './App.css';
-import { Login } from './Components/Login/Login';
-// import { Signup } from './Components/Signup/Signup'
+import { Login } from './Components/Login/Login'; // Corrected import for Login
+// import LoginButton from './Components/Login/Login.jsx'
+import LogoutButton from './Components/Login/Logout.jsx'
+import { useEffect } from 'react';
+import { gapi } from 'gapi-script'
+// import LogoutButton from './Components/Login/Logout'; // Imported but not used
+// import { Signup } from './Components/Signup/Signup'; // Commented out
+
+const clientId = "908871574694-acla8nrm33935unr11j8elvid4q6ea5p.apps.googleusercontent.com";
 
 function App() {
+
+  useEffect(()=>{
+    function start(){
+      gapi.client.init({
+        clientId: clientId,
+        scope:""
+      })
+    };
+
+    gapi.load('client:auth2', start);
+  });
   return (
-    <div>
+    <div className="App">
       <Login/>
-      {/* <Signup/> */}
+      {/* <LoginButton /> */}
+      {/* <LogoutButton />  */}
+      {/* <Signup/> Uncomment if you want to use the Signup component */}
     </div>
   );
 }
